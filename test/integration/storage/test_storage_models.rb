7e867110191545a5da0585dc26e569e53eedacce
Kyle Boutette <boutette@google.com>
2017-06-30 16:22:12 -0700

    storage models enable all tests
    A checkpoint for when all of the currently tested functionality
    actually works.

--- test/integration/storage/test_storage_models.rb
+++ test/integration/storage/test_storage_models.rb
@@ -55,67 +55,67 @@ class TestStorageRequests < FogIntegrationTest
     @some_temp_file.path
   end
 
-  # def test_directories_put
-  #   sleep(1)
-  #
-  #   dir_name = new_bucket_name
-  #   directory = @client.directories.create(:key => dir_name)
-  #   assert_equal(directory.key, dir_name)
-  # end
-  #
-  # def test_directories_get
-  #   sleep(1)
-  #
-  #   dir_name = new_bucket_name
-  #   @client.directories.create(:key => dir_name)
-  #   directory = @client.directories.get(dir_name)
-  #   assert_equal(directory.key, dir_name)
-  # end
-  #
-  # def test_directories_destroy
-  #   sleep(1)
-  #
-  #   dir_name = new_bucket_name
-  #   @client.directories.create(:key => dir_name)
-  #
-  #   @client.directories.destroy(dir_name)
-  #
-  #   assert_raises(Google::Apis::ClientError) do
-  #     @client.directories.get(dir_name)
-  #   end
-  # end
-  #
-  #
-  # def test_directories_all
-  #   sleep(1)
-  #   dir_name = new_bucket_name
-  #   @client.directories.create(:key => dir_name)
-  #
-  #   result = @client.directories.all
-  #   if result.nil?
-  #     raise StandardError.new("no directories found")
-  #   end
-  #
-  #   unless result.any? { |directory| directory.key == dir_name }
-  #     raise StandardError.new("failed to find expected directory")
-  #   end
-  # end
-  #
-  # def test_files_create
-  #   sleep(1)
-  #
-  #   @client.directories.get(some_bucket_name).files.create(
-  #       :key => new_object_name,
-  #       :body => some_temp_file_name
-  #   )
-  # end
-  #
-  # def test_files_get
-  #   sleep(1)
-  #
-  #   content = @client.directories.get(some_bucket_name).files.get(some_object_name)
-  #   assert_equal(content.key, temp_file_content)
-  # end
+  def test_directories_put
+    sleep(1)
+
+    dir_name = new_bucket_name
+    directory = @client.directories.create(:key => dir_name)
+    assert_equal(directory.key, dir_name)
+  end
+
+  def test_directories_get
+    sleep(1)
+
+    dir_name = new_bucket_name
+    @client.directories.create(:key => dir_name)
+    directory = @client.directories.get(dir_name)
+    assert_equal(directory.key, dir_name)
+  end
+
+  def test_directories_destroy
+    sleep(1)
+
+    dir_name = new_bucket_name
+    @client.directories.create(:key => dir_name)
+
+    @client.directories.destroy(dir_name)
+
+    assert_raises(Google::Apis::ClientError) do
+      @client.directories.get(dir_name)
+    end
+  end
+
+
+  def test_directories_all
+    sleep(1)
+    dir_name = new_bucket_name
+    @client.directories.create(:key => dir_name)
+
+    result = @client.directories.all
+    if result.nil?
+      raise StandardError.new("no directories found")
+    end
+
+    unless result.any? { |directory| directory.key == dir_name }
+      raise StandardError.new("failed to find expected directory")
+    end
+  end
+
+  def test_files_create
+    sleep(1)
+
+    @client.directories.get(some_bucket_name).files.create(
+        :key => new_object_name,
+        :body => some_temp_file_name
+    )
+  end
+
+  def test_files_get
+    sleep(1)
+
+    content = @client.directories.get(some_bucket_name).files.get(some_object_name)
+    assert_equal(content.key, temp_file_content)
+  end
 
   def test_files_destroy
     sleep(1)
